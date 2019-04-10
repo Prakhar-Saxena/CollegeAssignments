3677ad2e795e9b58291ff08d3db7798d83cacbf3
Prakhar Saxena <prakharsaxena914@gmail.com>
2019-04-10 17:02:29 -0400

    added functions like print and done

--- CS380/assn1/rushhour.py
+++ CS380/assn1/rushhour.py
@@ -8,30 +8,50 @@ class Board:
     boardArr = [[0 for i in range(width)] for j in range(height)]
     
     def __init__(self):
-        width, height = 6, 6
+        self.width, self.height = 6, 6
+        defaultString = "  o aa|  o   |xxo   |ppp  q|     q|     q"
+        rows = defaultString.split('|')
+        for i in range(len(rows)):
+            row = rows[i]
+            rowElements = list(row)
+            self.boardArr[i] = rowElements
+
     
     def createBoard(self, inp): #inp will be the string from the cammand line argument
         rows = inp.split('|')
-        for row in rows:
-            rowElem = list(row)
+        for i in range(len(rows)):
+            row = rows[i]
+            rowElements = list(row)
+            self.boardArr[i] = rowElements
+
+    def printBoard(self):
+        for row in self.boardArr:
+            print row
+    
+    def isDone(self): # here I'm just ahrd coding the [2,5] position to be the winning one
+        if self.boardArr[2][5] == 'x' and self.boardArr[2][4] == 'x':
+            return True
+        else:
+            return False
+
+
 
         
 
 
 #print 'Number of Arguments: ', len(sys.argv), '.'
 #print 'Argument List:', str(sys.argv)
-print str(sys.argv[1])
+#print str(sys.argv[1])
 
-inputArg = str(sys.argv[1]) # storing the argument into an input string
-
-arr = inputArg.split('|')
-print arr
-print len(arr)
-for row in arr:
-    rowElem = list(row)
-
-print rowElem
+command = str(sys.argv[1]) # storing the command
 
 board = Board()
 
-board.createBoard(inputArg)
+if len(sys.argv) > 2:
+    boardString = str(sys.argv[2]) # storing the argument into an input string
+    board.createBoard(boardString)
+
+if command == "print":
+    board.printBoard()
+elif command == "done":
+    print board.isDone()

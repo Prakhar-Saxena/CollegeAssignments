d1501172da83b29d5a6ed7eee6082f69f6d889f3
Prakhar Saxena <prakharsaxena914@gmail.com>
2019-04-10 21:22:13 -0400

    made certain necessary changes with print

--- CS380/assn1/rushhour.py
+++ CS380/assn1/rushhour.py
@@ -9,7 +9,7 @@ class Board:
     
     def __init__(self):
         self.width, self.height = 6, 6
-        defaultString = "  o aa|  o   |xxo   |ppp  q|     q|     q"
+        defaultString = "  o aa|  o   |xxo   |ppp  q|     q|     q" #default construction as directed
         rows = defaultString.split('|')
         for i in range(len(rows)):
             row = rows[i]
@@ -25,19 +25,23 @@ class Board:
             self.boardArr[i] = rowElements
 
     def printBoard(self):
-        for row in self.boardArr:
-            print row
+        print '  - - - - - -  '
+        for i in range(len(self.boardArr)):
+            row = self.boardArr[i]
+            print '|',
+            for element in row:
+                print element,
+            if i == 2: # exit position in the board
+                print ' '
+            else:
+                print '|'
+        print '  - - - - - -  '
     
     def isDone(self): # here I'm just ahrd coding the [2,5] position to be the winning one
         if self.boardArr[2][5] == 'x' and self.boardArr[2][4] == 'x':
             return True
         else:
             return False
-
-
-
-        
-
 
 #print 'Number of Arguments: ', len(sys.argv), '.'
 #print 'Argument List:', str(sys.argv)
@@ -47,7 +51,7 @@ command = str(sys.argv[1]) # storing the command
 
 board = Board()
 
-if len(sys.argv) > 2:
+if len(sys.argv) > 2: # checking whether there's the optional argument for input
     boardString = str(sys.argv[2]) # storing the argument into an input string
     board.createBoard(boardString)
 

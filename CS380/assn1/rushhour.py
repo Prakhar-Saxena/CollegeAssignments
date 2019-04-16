c32179da3be9c0bc0b208d399e09a953e308fdcf
Prakhar Saxena <prakharsaxena914@gmail.com>
2019-04-16 09:56:17 -0400

    added find and findall method, also added a test script, well to test things..

--- CS380/assn1/rushhour.py
+++ CS380/assn1/rushhour.py
@@ -3,6 +3,8 @@
 import sys
 import math
 
+# add a CLOSED set for duplicates/ or for states that we've already achieved
+
 class Board:
     width, height = 6, 6
     boardArr = [[0 for i in range(width)] for j in range(height)]
@@ -38,14 +40,52 @@ class Board:
         print '  - - - - - -  '
     
     def isDone(self): # here I'm just ahrd coding the [2,5] position to be the winning one
-        if self.boardArr[2][5] == 'x' and self.boardArr[2][4] == 'x':
+        if self.boardArr[2][5] == 'x' and self.boardArr[2][4] == 'x': # assuming the car is just two characters long
             return True
         else:
             return False
 
+    def next_for_car(self, car): # can take arguments as character
+        for row in self.boardArr:
+            if car not in row:
+                print 'car not in the baord'
+                break
+                return
+        
+
+def find(l, elem): # https://stackoverflow.com/questions/6518291/using-index-on-multidimensional-lists
+    for row, i in enumerate(l):
+        try:
+            column = i.index(elem)
+        except ValueError:
+            continue
+        return row, column
+    return -1
+
+def findall(l, elem):
+    returnList = []
+    c = find(l, elem)
+    while c != -1:
+        x = c[0]
+        y = c[1]
+        returnList.append([x,y])
+        l[x][y] = 'boi'
+        c = find(l, elem)
+    return returnList
+
+
+class Car:
+    end1 = [0,0]
+    end2 = [0,0]
+    length = 0
+
+    def __init__(self, board, car): # takes the board and the car character
+        
+
 #print 'Number of Arguments: ', len(sys.argv), '.'
 #print 'Argument List:', str(sys.argv)
 #print str(sys.argv[1])
+
 
 command = str(sys.argv[1]) # storing the command
 

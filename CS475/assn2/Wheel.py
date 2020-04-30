f174c1129bbeb0f34856f66173122526e378c63c
Prakhar Saxena <prakharsaxena914@gmail.com>
2020-04-29 21:25:34 -0400

    stuff

--- CS475/assn2/Wheel.py
+++ CS475/assn2/Wheel.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 class Wheel:
-    def __init__(self, cin, cout):  # pun intended
+    def __init__(self, cin, cout, orientation):  # pun unintended: cin and cout refer to character in and out
         if len(cin) != len(cout):
             return
-        self.wiring = dict(zip(cin,cout))
+        self.wiring = dict(zip(cin, cout))
+        self.orientation = orientation
+        self.inWheel = cin
+        self.outWheel = cout
 
     def getCout(self, cin):
         return self.wiring[cin]
+
+    def turn(self):
+        self.inWheel = self.inWheel[1:] + [self.inWheel[0]]
+        self.outWheel = self.outWheel[1:] + [self.outWheel[0]]

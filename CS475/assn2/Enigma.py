7367147abf33100e6a34a5ff49b86cc02dcfdf48
Prakhar Saxena <prakharsaxena914@gmail.com>
2020-05-01 20:07:47 -0400

    CS475 enigma stuff

--- CS475/assn2/Enigma.py
+++ CS475/assn2/Enigma.py
@@ -5,11 +5,13 @@ from Wheel import Wheel
 
 class Enigma:
     def __init__(self):
+        self.wheel = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         self.leftWheel = Wheel(
             ['2', 'Y', 'Z', '0', '1', 'A', 'W', 'I', 'P', 'K', 'S', 'N', '3', 'T', 'E', 'R', 'M', 'U', 'C', '5', 'V',
              '6', 'X', '7', 'F', 'Q', 'O', 'L', '4', '8', 'G', 'D', '9', 'B', 'J', 'H'],
             0
-            )
+        )
         self.middleWheel = Wheel(
             ['0', 'L', 'X', '1', '2', '8', 'H', 'B', '3', 'N', 'R', 'O', 'K', 'D', 'T', '7', 'C', '6', 'P', 'I', 'V',
              'J', '4', 'A', 'U', 'W', 'M', 'E', '9', '5', 'Q', 'S', 'Z', 'G', 'Y', 'F'],
@@ -23,11 +25,30 @@ class Enigma:
         self.characterCounter = 0
 
     def getChout(self, chin):
-        rightOut_midIn = self.rightWheel.getChout(chin)
+        rightOut_midIn = self.rightWheel.getContactOut(self.wheel.index(chin))
         self.rightWheel.turn()
-        midOut_leftIn = self.rightWheel.getChout(rightOut_midIn)
+        midOut_leftIn = self.rightWheel.getContactOut(rightOut_midIn)
         if self.characterCounter % 7 == 0:
             self.middleWheel.turn()
-        leftOut = self.leftWheel.getChout(midOut_leftIn)
+        leftOut = self.leftWheel.getContactOut(midOut_leftIn)
         if self.characterCounter % 5 == 0:
             self.leftWheel.turn()
+        return self.wheel[leftOut]
+
+
+def main():
+    # wheel = Wheel(
+    #     ['2', 'Y', 'Z', '0', '1', 'A', 'W', 'I', 'P', 'K', 'S', 'N', '3', 'T', 'E', 'R', 'M', 'U', 'C', '5', 'V',
+    #      '6', 'X', '7', 'F', 'Q', 'O', 'L', '4', '8', 'G', 'D', '9', 'B', 'J', 'H'],
+    #     0
+    # )
+    # wheel.turn()
+    # print(wheel.getContactOut(0))
+    enigma = Enigma()
+    print(enigma.getChout('C'))
+    print(enigma.getChout('C'))
+    print(enigma.getChout('I'))
+
+
+if __name__ == '__main__':
+    main()
--- CS475/assn2/Wheel.py
+++ CS475/assn2/Wheel.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python3
 
 class Wheel:
-    def __init__(self, chin, chout, orientation):  # chin and chout refer to character in and out
-        if len(chin) != len(chout):
+    def __init__(self, chout, orientation):  # chin and chout refer to character in and out
+        if len(chout) != 36:
             return
-        self.wiring = dict(zip(chin, chout))
-        self.orientation = orientation
         self.wheel = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
                       'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        self.baseWheel = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-                      'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        self.wiring = dict(zip(self.wheel[:], chout))
+        self.orientation = orientation
+
+        # self.baseWheel = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+        #               'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
     def getChout(self, chin):
         return self.wiring[chin]
 
+    def getContactOut(self, contactIn):
+        chin = self.wheel[contactIn + self.orientation]  # because the wheel has turned self.orienteation turns, so the character in is the contactIn + orientation
+        chout = self.getChout(chin)
+        contactOut = self.wheel.index(chout) - self.orientation
+        if contactOut < 0:
+            contactOut += 36
+        return contactOut
+
     def turn(self):
-        self.wheel = self.wheel[1:] + [self.wheel[0]]
+        # self.wheel = self.wheel[1:] + [self.wheel[0]]
         self.orientation += 1
         self.orientation = self.orientation % 36
 
     def turnOtherWay(self):
-        self.wheel = [self.wheel[-1]] + self.wheel[:-1]
+        # self.wheel = [self.wheel[-1]] + self.wheel[:-1]
         self.orientation -= 1
         if self.orientation == -1:
             self.orientation = 35

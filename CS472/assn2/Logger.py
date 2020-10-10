ea095b210dc1ba540862c2643b682ce49fe9d6b2
Prakhar Saxena <prakharsaxena914@gmail.com>
2020-10-10 17:43:01 -0400

    more changes

--- CS472/assn2/Logger.py
+++ CS472/assn2/Logger.py
@@ -4,32 +4,33 @@ import sys
 import datetime
 
 class Logger:
-    f = None
-
-    @staticmethod
-    def log_file_init(file_name):
+    def __init__(self, log_file_name):
         try:
-            Logger.f = open(file_name, "a")
-        except (IOError, Exception) as err:
-            print("Logging onto file exception.")
+            self.log_file_name = log_file_name
+            self.f = open(log_file_name, 'a')
+        except Exception as e:
+            print(str(e))
             sys.exit(0)
 
-    @staticmethod
-    def log(msg):
-        Logger.f.write(str(datetime.datetime.now()) + '\t' + msg + '\n')
+    def close_file(self):
+        try:
+            self.f.close()
+        except Exception as e:
+            print(str(e))
+            sys.exit(0)
 
-    @staticmethod
-    def log_err(err_msg):
-        Logger.f.write(str(datetime.datetime.now()) + '\t' + 'X'*25 + 'ERROR' + 'X'*25 + '\n\t\t\t' + err_msg + '\n')
+    def log(self, msg):
+        self.f.write(str(datetime.datetime.now()) + '\t' + msg + '\n')
 
-    @staticmethod
-    def log_socket_error(error_msg):  # created this because Socket error is the most expected/caught error.
-        Logger.log_err('Socket Error: ' + error_msg)
+    def log_err(self, err_msg):
+        self.f.write(str(datetime.datetime.now()) + '\t' + 'X'*25 + 'ERROR' + 'X'*25 + '\n\t\t\t' + err_msg + '\n')
+        print('Something didn\'t work and now the whole thing is blown.')
 
-    @staticmethod
-    def log_response(response):
-        Logger.log('Response Received: ' + response)
+    def log_socket_error(self, error_msg):  # created this because Socket error is the most expected/caught error.
+        self.log_err('Socket Error: ' + error_msg)
 
-    @staticmethod
-    def log_attempt(msg):
-        Logger.log('Attempting ' + msg)
+    def log_response(self, response):
+        self.log('Response Received: ' + response)
+
+    def log_attempt(self, msg):
+        self.log('Attempting ' + msg)
--- CS472/assn2/ftpclient.py
+++ CS472/assn2/ftpclient.py
@@ -4,7 +4,6 @@ import socket
 import sys
 from Logger import Logger
 
-
 class FtpClient:
     def __init__(self, ip, port):
         self.ip = ip
@@ -25,7 +24,7 @@ class FtpClient:
     def response(self):
         response = str(self.s.recv(1024)).strip()
         print(response)
-        Logger.log_response(response)
+        logger.log_response(response)
         return response
 
     @staticmethod
@@ -34,92 +33,126 @@ class FtpClient:
 
     def connect_server(self):
         try:
-            Logger.log_attempt('server connection with sockets.')
+            logger.log_attempt('server connection with sockets.')
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            Logger.log('Socket created.')
+            logger.log('Socket created.')
             self.s.connect((self.ip, int(self.port)))
             response = self.response()
-            Logger.log('Socket Successfully connected to ' + str(self.ip) + ' at port ' + str(self.ip))
+            logger.log('Socket Successfully connected to ' + str(self.ip) + ' at port ' + str(self.ip))
         except socket.error as e:
-            Logger.log_socket_error(str(e))
+            logger.log_socket_error(str(e))
+        except Exception as e:
+            logger.log_err(str(e))
 
     def user_command(self):
         try:
-            Logger.log_attempt('USER')
+            logger.log_attempt('USER')
             user_name = input('Enter username: ')
             self.s.send(FtpClient.str_to_bytes('USER ' + user_name + '\n'))
-            Logger.log('Sent: USER - ' + user_name)
+            logger.log('Sent: USER - ' + user_name)
             response = self.response()
             self.pass_command()
         except socket.error as e:
-            Logger.log_socket_error(str(e))
+            logger.log_socket_error(str(e))
+        except Exception as e:
+            logger.log_err(str(e))
 
     def pass_command(self):
         try:
-            Logger.log_attempt('PASS')
+            logger.log_attempt('PASS')
             password = input('Enter password: ')
             self.s.send(FtpClient.str_to_bytes('PASS ' + password + '\n'))
-            Logger.log('Sent: PASS ' + password)
+            logger.log('Sent: PASS ' + password)
             response = self.response()
         except socket.error as e:
-            Logger.log_socket_error(str(e))
+            logger.log_socket_error(str(e))
+        except Exception as e:
+            logger.log_err(str(e))
 
     def cwd_command(self):
         try:
-            Logger.log_attempt('CWD')
+            logger.log_attempt('CWD')
             cd_input = input('Enter directory: ')
             self.s.send(FtpClient.str_to_bytes('CWD ' + cd_input + '\n'))
-            Logger.log('Sent: CWD ' + cd_input)
+            logger.log('Sent: CWD ' + cd_input)
             response = self.response()
             self.pwd_command()
         except Exception as e:
-            Logger.log_err('Error: ' + str(e))
+            logger.log_err('Error: ' + str(e))
         return
 
     def quit_command(self):
         try:
-            Logger.log_attempt('QUIT')
+            logger.log_attempt('QUIT')
             self.s.send(FtpClient.str_to_bytes('QUIT \n'))
-            Logger.log('Sent: QUIT')
+            logger.log('Sent: QUIT')
             response = self.response()
             print('Quiting.')
-            Logger.log('Quiting.')
+            logger.log('Quiting.')
             self.s.close()
+            logger.close_file()
             sys.exit(0)
         except socket.error as e:
-            Logger.log_socket_error(str(e))
+            logger.log_socket_error(str(e))
+        except Exception as e:
+            logger.log_err(str(e))
 
     def pasv_command(self):
         try:
-            Logger.log_attempt('PASV')
+            logger.log_attempt('PASV')
             if self.is_passive:
                 self.is_passive = False
             elif not self.is_passive:
                 self.is_passive = True
             else:
                 print('This should be impossible.')
-                Logger.log('This should be impossible.')
+                logger.log('This should be impossible.')
         except Exception as e:
-            Logger.log_err(str(e))
+            logger.log_err(str(e))
 
     def epsv_command(self):
-        return
+        try:
+            logger.log_attempt('EPSV')
+            self.s.send(FtpClient.str_to_bytes('EPSV \n'))
+            response = self.response()
+            port = response.split('|')[3]
+            socket_rec = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            logger.log('New Socket created, for data receiving.')
+            socket_rec.connect((self.ip, port))
+            socket_rec.close()
+        except socket.error as e:
+            logger.log_socket_error(str(e))
+        except Exception as e:
+            logger.log_err(str(e))
 
     def port_command(self):
         try:
-            Logger.log_attempt('PORT')
+            logger.log_attempt('PORT')
             if self.is_port:
                 self.is_port = False
             elif not self.is_port:
                 self.is_port = True
             else:
                 print('This should be impossible.')
-                Logger.log('This should be impossible.')
+                logger.log('This should be impossible.')
         except Exception as e:
-            Logger.log_err(str(e))
+            logger.log_err(str(e))
 
     def eprt_command(self):
-        return
+        try:
+            socket_rec = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket_rec.bind(socket.gethostbyname(socket.gethostbyname(), 0))
+            socket_rec.listen(1)
+            logger.log('New Socket created, for data receiving.')
+            port = str(socket_rec.getsockname()[1])
+            ip = str(socket.gethostbyname(socket.gethostbyname()))
+            self.s.send('EPRT | 1 | ' + ip + ' | ' + port + '\n')
+            logger.log('Sent: EPRT')
+            response = self.response()
+        except socket.error as e:
+            logger.log_socket_error(str(e))
+        except Exception as e:
+            logger.log_err(str(e))
 
     def retr_command(self):
         return
@@ -129,12 +162,14 @@ class FtpClient:
 
     def pwd_command(self):
         try:
-            Logger.log_attempt('PWD')
+            logger.log_attempt('PWD')
             self.s.send(FtpClient.str_to_bytes('PWD \n'))
-            Logger.log('Sent: PWD')
+            logger.log('Sent: PWD')
             response = self.response()
         except socket.error as e:
-            Logger.log_socket_error(str(e))
+            logger.log_socket_error(str(e))
+        except Exception as e:
+            logger.log_err(str(e))
 
     def syst_command(self):
         return
@@ -144,15 +179,15 @@ class FtpClient:
 
     def help_command(self):
         try:
-            Logger.log_attempt('HELP')
+            logger.log_attempt('HELP')
         except socket.error as e:
-            Logger.log_socket_error(str(e))
+            logger.log_socket_error(str(e))
 
     '''
     REPL: Read-Evaluate-Print-Loop
     '''
     def menu_repl(self):
-        Logger.log('Initialising menu REPL.')
+        logger.log('Initialising menu REPL.')
         print('Hello there!\n You\'re in the menu REPL now.')
         print('-' * 25)
         while True:
@@ -197,7 +232,7 @@ class FtpClient:
                 self.help_command()
             else:
                 print('Invalid command entered. Try again.\n')
-                Logger.log('Invalid command entered: ' + user_choice)
+                logger.log('Invalid command entered: ' + user_choice)
                 print('-' * 25)
 
 
@@ -219,16 +254,18 @@ def main():
     ip = sys.argv[1]
     log_file_name = sys.argv[2]
 
-    Logger.log_file_init(log_file_name)
-    Logger.log('-' * 25 + 'STARTS HERE' + '-' * 25)
-    Logger.log('New ftp client intialised.')
-    Logger.log('IP: ' + ip)
-    Logger.log('Port: ' + port)
+    global logger
+    logger = Logger(log_file_name)
+
+    logger.log('-' * 25 + 'STARTS HERE' + '-' * 25)
+    logger.log('New ftp client intialised.')
+    logger.log('IP: ' + ip)
+    logger.log('Port: ' + port)
 
     ftp_client = FtpClient(ip, port)
     ftp_client.initialise()
 
-    Logger.log('-' * 25 + 'STOPS HERE' + '-' * 25)
+    logger.log('-' * 25 + 'STOPS HERE' + '-' * 25)
 
 
 if __name__ == '__main__':
